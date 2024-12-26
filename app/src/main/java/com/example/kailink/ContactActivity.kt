b2fdc21442efa1a20778e1c7f66f6418b7f69802
jmnew03 <jmnew03@kaist.ac.kr>
2024-12-26 19:48:11 +0900

    feature; Added ContactActivity
    - activity 생성
    - toolbar 삽입

---
+++ app/src/main/java/com/example/kailink/ContactActivity.kt
@@ -0,0 +1,40 @@
+package com.example.kailink
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.kailink.databinding.ActivityContactBinding
+import androidx.appcompat.widget.Toolbar
+
+class ContactActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityContactBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityContactBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Set up the Toolbar as the ActionBar
+        val toolbar = binding.root.findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_contact)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
