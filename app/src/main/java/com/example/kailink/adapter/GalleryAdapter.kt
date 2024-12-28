2d02dae7f90b5f2658e5a44a246a752fe0adcefc
Yoon <ysj0509@sookmyung.ac.kr>
2024-12-28 17:17:58 +0900

    Feat: implement gallery model and change from dashboardItem to the gallery model
    - Gallery.kt를 통해 {이미지, 번호, 이름, 별칭} 구조 정의
    - 기존 {이미지, 텍스트} 구조의 dashboard를 받던 Adapter에서 텍스트 삭제 후,
      {num, name, alias} 추가

--- app/src/main/java/com/example/kailink/adapter/GalleryAdapter.kt
+++ app/src/main/java/com/example/kailink/adapter/GalleryAdapter.kt
@@ -8,13 +8,16 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kailink.R
 import com.example.kailink.model.Contact
+import com.example.kailink.model.Gallery
 
-class GalleryAdapter(private val items: List<DashboardItem>) :
+class GalleryAdapter(private val items: List<Gallery>) :
     RecyclerView.Adapter<GalleryAdapter.DashboardViewHolder>() {
 
     class DashboardViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val imageView: ImageView = view.findViewById(R.id.imageView_card)
-        val textView: TextView = view.findViewById(R.id.textView_card)
+        val galleryNumTextView: TextView = view.findViewById(R.id.gallery_num)
+        val galleryNameTextView: TextView = view.findViewById(R.id.gallery_name)
+        val galleryAliasTextView: TextView = view.findViewById(R.id.gallery_alias)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DashboardViewHolder {
@@ -25,11 +28,16 @@ class GalleryAdapter(private val items: List<DashboardItem>) :
 
     override fun onBindViewHolder(holder: DashboardViewHolder, position: Int) {
         val item = items[position]
-        holder.imageView.setImageResource(item.imageResId)
-        holder.textView.text = item.text
+
+        val resourceId = holder.itemView.context.resources.getIdentifier(item.image, "drawable", holder.itemView.context.packageName)
+        holder.imageView.setImageResource(resourceId)
+
+        holder.galleryNumTextView.text = item.galleryNum
+        holder.galleryNameTextView.text = item.galleryName
+        holder.galleryAliasTextView.text = item.galleryAlias
     }
 
     override fun getItemCount(): Int = items.size
 }
 
-data class DashboardItem(val imageResId: Int, val text: String)
+data class DashboardItem(val imageResId: Int, val galleryNum: String, val galleryName: String, val galleryAlias: String)
